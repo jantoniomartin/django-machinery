657f6fd9f1cd551a9da6f0ea0bef472fde9a02cb
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2014-02-24 12:53:26 +0100

    set default url to company list

--- crm/urls.py
+++ crm/urls.py
@@ -5,6 +5,14 @@ from django.views.generic.edit import CreateView, UpdateView
 from crm.models import Company, Group, Department
 
 urlpatterns = patterns("djinn_crm.views",
+    url(r'^$',
+		ListView.as_view(
+			model=Company,
+        	paginate_by=10,
+			context_object_name="company_list"
+		),
+		name="crm_company_list"
+	),
 	url(r'^group/list/$',
 		ListView.as_view(
 			model=Group,
@@ -31,14 +39,6 @@ urlpatterns = patterns("djinn_crm.views",
 			model = Group
 		),
 		name="crm_group_create"
-	),
-    url(r'^company/list/$',
-		ListView.as_view(
-			model=Company,
-        	paginate_by=10,
-			context_object_name="company_list"
-		),
-		name="crm_company_list"
 	),
     url(r'^company/detail/(?P<pk>\d+)/$',
 		DetailView.as_view(
