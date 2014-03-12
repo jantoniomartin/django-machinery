8d6586e9363f71b08f84ab1966302980bc951dfd
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2014-03-12 13:32:25 +0100

    add user authentication

---
+++ indumatic/middleware.py
@@ -0,0 +1,18 @@
+from re import compile
+
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
+EXEMPT_URLS = [compile(settings.LOGIN_URL.lstrip('/'))]
+if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
+	EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
+
+class LoginRequiredMiddleware:
+	def process_request(self, request):
+		assert hasattr(request, 'user'), "The Login Required middleware\
+		requires authentiation middleware to be installed."
+		if not request.user.is_authenticated():
+			path = request.path_info.lstrip('/')
+			if not any(m.match(path) for m in EXEMPT_URLS):
+				return HttpResponseRedirect(settings.LOGIN_URL)
+
