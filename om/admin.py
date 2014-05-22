6ecbb0277be27088c7c7df27ba2899ac595cfd0f
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2014-05-22 09:28:12 +0200

    in admin, show order items as order inlines

--- om/admin.py
+++ om/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from om.models import *
 
+class OrderItemInline(admin.TabularInline):
+	model = OrderItem
+	fields = (
+		'article_code',
+		'ordered_quantity',
+		'received_quantity',
+		'completed_on',
+		'estimated_delivery',)
+	
+	readonly_fields = ('article_code',)
+
+	def article_code(self, instance):
+		return unicode(instance.offer.article)
+
+	article_code.short_description = _("Article code")
+
+class OrderAdmin(admin.ModelAdmin):
+	inlines = [OrderItemInline,]
+
 admin.site.register(Offer)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(CartItem)
 admin.site.register(CsvTemplate)
